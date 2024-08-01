ab3cdd8542d98142745864e8667727aa075bb6b3
pedroggwp <phsnnascimento@outlook.com>
2024-08-01 10:28:38 -0300

    permissões de câmera

--- CameraGallery/app/src/main/java/com/example/cameragallery/MainActivity.java
+++ CameraGallery/app/src/main/java/com/example/cameragallery/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.cameragallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Request da permissão
+        if (allPermisionsFranted()) {
+            // Ativar câmera 
+        } else {
+            requestPermissions();
+        }
+    }
+
+    private boolean allPermisionsFranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
