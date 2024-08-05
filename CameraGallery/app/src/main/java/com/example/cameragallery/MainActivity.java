e63016a3237ad3d70608c737cba8c827f708dcda
pedroggwp <phsnnascimento@outlook.com>
2024-08-05 12:30:57 -0300

    adicionando permissões de câmera

--- CameraGallery/app/src/main/java/com/example/cameragallery/MainActivity.java
+++ CameraGallery/app/src/main/java/com/example/cameragallery/MainActivity.java
@@ -1,12 +1,43 @@
 package com.example.cameragallery;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] REQUIRED_PERMISSIONS = {
+            android.Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+    };
+
+    private final ActivityResultLauncher<String[]> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestMultiplePermissions(),
+            permissions -> {
+                // Handle Permission granted/rejected
+                boolean permissionGranted = true;
+                for (Map.Entry<String, Boolean> entry : permissions.entrySet()) {
+                    if (Arrays.asList(REQUIRED_PERMISSIONS).contains(entry.getKey()) && !entry.getValue()) {
+                        permissionGranted = false;
+                        break;
+                    }
+                }
+
+                if (!permissionGranted) {
+                    Toast.makeText(getApplicationContext(),"Permissão NEGADA.",Toast.LENGTH_SHORT).show();
+                } else {
+                    // ok acessos liberados ;
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +45,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         // Request da permissão
-        if (allPermisionsFranted()) {
+        if (allPermissionsGranted()) {
             // Ativar câmera 
         } else {
             requestPermissions();
         }
     }
 
-    private boolean allPermisionsFranted() {
+    private boolean allPermissionsGranted() {
         for (String permission : REQUIRED_PERMISSIONS) {
             if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
                 return false;
@@ -29,5 +60,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private void requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS);
     }
 }
