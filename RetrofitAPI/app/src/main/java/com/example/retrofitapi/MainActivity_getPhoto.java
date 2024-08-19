52bdd4a775cdbdc736b429b7daec2b8a58c151c1
pedroggwp <phsnnascimento@outlook.com>
2024-08-19 12:24:53 -0300

    projeto request com lib Retrofit

---
+++ RetrofitAPI/app/src/main/java/com/example/retrofitapi/MainActivity_getPhoto.java
@@ -0,0 +1,14 @@
+package com.example.retrofitapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_getPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_get_photo);
+    }
+}
