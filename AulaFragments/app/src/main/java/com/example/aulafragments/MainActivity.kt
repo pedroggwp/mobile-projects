559a3838c2cd8918388e9ace5e8c22ac182c0b04
pedroggwp <phsnnascimento@outlook.com>
2024-09-15 17:37:56 -0300

    limpando projeto

--- AulaFragments/app/src/main/java/com/example/aulafragments/MainActivity.kt
+++ AulaFragments/app/src/main/java/com/example/aulafragments/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.aulafragments
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.aulafragments.databinding.ActivityMainBinding
 import com.example.aulafragments.fragments.ChamadasFragment
 import com.example.aulafragments.fragments.ConversasFragment
@@ -21,16 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        Log.i("ciclo_vida", "Activity onCreate")
-
-
-//        val fragmentManager = supportFragmentManager.beginTransaction()
-//
-//        // Diversas alterações em fragments
-//        fragmentManager.add(binding.fragmentConteudo.id, ConversasFragment())
-//
-//        fragmentManager.commit()
 
         btnConversas = binding.btnConversas
         btnChamadas = binding.btnChamadas
@@ -51,28 +37,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("ciclo_vida", "Activity onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("ciclo_vida", "Activity onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("ciclo_vida", "Activity onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("ciclo_vida", "Activity onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("ciclo_vida", "Activity onDestroy")
-    }
 }
--- AulaFragments/app/src/main/java/com/example/aulafragments/fragments/ConversasFragment.kt
+++ AulaFragments/app/src/main/java/com/example/aulafragments/fragments/ConversasFragment.kt
@@ -1,77 +1,37 @@
 package com.example.aulafragments.fragments
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.aulafragments.databinding.FragmentConversasBinding
 
 class ConversasFragment : Fragment() {
 
     private lateinit var binding: FragmentConversasBinding
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        Log.i("ciclo_vida", "Fragment onAttach")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.i("ciclo_vida", "Fragment onCreate")
-    }
+    private lateinit var btnExecutar: Button
+    private lateinit var textNome: TextView
+    private lateinit var editTextNome: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        Log.i("ciclo_vida", "Fragment onCreateView")
         binding = FragmentConversasBinding.inflate(layoutInflater, container, false)
 
+        btnExecutar = binding.btnExecutar
+        textNome = binding.textNome
+        editTextNome = binding.editTextNome
+
+        btnExecutar.setOnClickListener {
+            textNome.text = editTextNome.text.toString()
+        }
 
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        Log.i("ciclo_vida", "Fragment onViewCreated")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i("ciclo_vida", "Fragment onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("ciclo_vida", "Fragment onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("ciclo_vida", "Fragment onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("ciclo_vida", "Fragment onStop")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.i("ciclo_vida", "Fragment onDestroyView")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("ciclo_vida", "Fragment onDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.i("ciclo_vida", "Fragment onDetach")
     }
 }
