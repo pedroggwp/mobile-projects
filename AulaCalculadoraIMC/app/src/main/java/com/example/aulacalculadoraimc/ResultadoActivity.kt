8e3120e9e78f3f385e7060617f52e80fcdcbefb8
pedroggwp <phsnnascimento@outlook.com>
2024-09-15 15:01:45 -0300

    removendo importações nao usadas

--- AulaCalculadoraIMC/app/src/main/java/com/example/aulacalculadoraimc/ResultadoActivity.kt
+++ AulaCalculadoraIMC/app/src/main/java/com/example/aulacalculadoraimc/ResultadoActivity.kt
@@ -2,10 +2,7 @@ package com.example.aulacalculadoraimc
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.aulacalculadoraimc.databinding.ActivityResultadoBinding
 
 class ResultadoActivity : AppCompatActivity() {
